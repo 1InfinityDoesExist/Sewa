0e2f66969a49af27929da21b023dab14462cfe3c
Avinash Patel <patelavinash508@gmail.com>
2022-04-18 01:08:39 +0530

    spring security added

--- src/main/java/com/patel/org/sewa/controller/SearchController.java
+++ src/main/java/com/patel/org/sewa/controller/SearchController.java
@@ -2,8 +2,11 @@ package com.patel.org.sewa.controller;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,7 +15,16 @@ import com.patel.org.sewa.model.response.SearchResponse;
 
 public interface SearchController {
 
+	// @PreAuthorize("hasAuthority('ROLE_ADMIN')") //roles - AMDIN
+	// @PreAuthorize("hasAuthority('admin')") //authorities - ROLE_admin
+	// @PreAuthorize("hasRole('ROLE_ADMIN')") // roles - ADMIN
+	@PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN', 'SUPER_ADMIN1')") // roles- ADMIN
+	// @PreAuthorize("hasRole('ADMIN') and hasRole('SUPER_ADMIN') and
+	// hasRole('SUPER_ADMIN1')") // roles - ADMIN
+	// @PreAuthorize("hasRole('ADMIN') or hasRole('SUPER_ADMIN') or
+	// hasRole('SUPER_ADMIN1')") // roles - ADMIN
+	// @PreAuthorize("hasRole('admin')")//authorities - ROLE_admin
 	@PostMapping(path = "/search")
-	public ResponseEntity<SearchResponse> searchUsingPOST(@RequestBody SearchRequest request, Pageable pageable)
-			throws IOException;
+	public ResponseEntity<SearchResponse> searchUsingPOST(HttpServletRequest req, @RequestBody SearchRequest request,
+			Pageable pageable) throws IOException;
 }
